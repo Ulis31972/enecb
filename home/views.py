645d1cf00a4da85da18f42b4da145521088cc48f
JoseDanielVelazquezHerrera <117121639+Urzlock@users.noreply.github.com>
2023-08-11 00:08:04 -0600

    Gafete funcional en navbar

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+def credencial(request):
+    return render(request, 'credenciales/credencialesFront.html')
+
