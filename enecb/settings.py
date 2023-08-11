b403dc2f66a3f69d577b18800f44fb0c5aa18342
Ulis3197 <19121072@morelia.tecnm.mx>
2023-08-10 19:08:45 -0600

    conectar pg

--- enecb/settings.py
+++ enecb/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'enecb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'tJTkilq8A1gE2JVCr7WQ',
+        'HOST': 'containers-us-west-126.railway.app',
+        'PORT': '5523',
     }
 }
 
