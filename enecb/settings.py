5709a54b14fdd266f48a85f998606591504b334b
Ulis3197 <62312455+Ulis3197@users.noreply.github.com>
2023-11-07 11:46:22 -0600

    pgadmin

--- enecb/settings.py
+++ enecb/settings.py
@@ -91,10 +91,21 @@ WSGI_APPLICATION = 'enecb.wsgi.application'
 #     )
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'enecb',
+        'USER': 'enecbUser',
+        'PASSWORD': 'enecbpass',
+        'HOST': 'localhost',  
+        'PORT': '5432',           
     }
 }
 
--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import InformacionExtraUsuario,Tecnologico
+from .models import *
 # Register your models here.
 
 # admin.site.register(ComplementoUser)
@@ -8,3 +8,4 @@ from .models import InformacionExtraUsuario,Tecnologico
 
 admin.site.register(InformacionExtraUsuario)
 admin.site.register(Tecnologico)
+admin.site.register(Hotel)
